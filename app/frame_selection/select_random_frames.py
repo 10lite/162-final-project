f59e5876fea4ea57e0f72fec4d5ac8a3807e2662
ASPactores <anakin.pactores@gmail.com>
2024-11-30 16:30:58 +0800

    feat: added image enhancement using CLAHE

--- app/frame_selection/select_random_frames.py
+++ app/frame_selection/select_random_frames.py
@@ -1,30 +1,24 @@
 import base64
-from io import BytesIO
-
 
 from fastapi import HTTPException, UploadFile, APIRouter, File
 from fastapi.responses import JSONResponse, StreamingResponse
-from .utils import extract_random_frames, compress_photo
+from .utils import extract_random_frames, compress_photo, enhance_photo
 
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import StreamingResponse
-from typing import Optional
+from fastapi import File, UploadFile
 
 
 router = APIRouter()
-from io import BytesIO
 
 
-@router.post("/select_random_frames")
+@router.post("/get_enhanced_frames")
 async def select_random_frames(video: UploadFile = File(...)):
     frames = []
 
     try:
         async for frame in extract_random_frames(video, number_of_frames=3):
-            compressed_frame = compress_photo(frame)
-            frames.append(
-                compressed_frame.getvalue()
-            )  # Get raw bytes from the BytesIO object
+            enhanced_image = enhance_photo(frame)
+            compressed_frame = compress_photo(enhanced_image)
+            frames.append(compressed_frame.getvalue())
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
@@ -34,32 +28,3 @@ async def select_random_frames(video: UploadFile = File(...)):
 
     # Return frames as a JSON response
     return JSONResponse(content={"photos": frames_base64})
-
-
-import io
-import decord
-from PIL import Image
-
-
-@router.post("/uploadvideo/")
-async def upload_video(video: UploadFile = File(...), frame: int = 0):
-    # Read the video data into memory
-    video_data = await video.read()  # Asynchronous read
-    video_stream = BytesIO(video_data)
-
-    # Use decord to read the video
-    container = decord.VideoReader(video_stream)
-
-    # Retrieve the specified frame
-    frame_data = container[frame].asnumpy()
-
-    # Convert the frame to a PIL image
-    image = Image.fromarray(frame_data)
-
-    # Save the image to a BytesIO buffer as JPEG
-    buffer = BytesIO()
-    image.save(buffer, format="JPEG")
-    buffer.seek(0)
-
-    # Return the frame as an image response
-    return StreamingResponse(buffer, media_type="image/jpeg")
--- app/frame_selection/utils.py
+++ app/frame_selection/utils.py
@@ -1,39 +1,23 @@
 from io import BytesIO
 from fastapi import UploadFile, File
+
 import random
 import decord
 import cv2
 import numpy as np
 
 
-# async def extract_random_frames(video_file: UploadFile, number_of_frames: int = 1):
-#     video_data = await video_file.read()
-#     video_stream = BytesIO(video_data)
+def enhance_photo(photo: np.ndarray) -> np.ndarray:
+    """
+    Apply CLAHE (Contrast Limited Adaptive Histogram Equalization) to enhance the photo.
+    """
+    hsv_photo = cv2.cvtColor(photo, cv2.COLOR_RGB2HSV)
 
-#     container = decord.VideoReader(video_stream)
-#     total_frames = len(container)
+    clahe = cv2.createCLAHE(clipLimit=5.0, tileGridSize=(8, 8))
+    hsv_photo[:, :, 2] = clahe.apply(hsv_photo[:, :, 2])
+    image_enhanced = cv2.cvtColor(hsv_photo, cv2.COLOR_HSV2RGB)
 
-#     frame_indices = random.sample(range(total_frames), number_of_frames)
-#     for index in frame_indices:
-#         frame_data = container[index].asnumpy()
-#         yield frame_data
-
-
-def enhance_photo(photo: BytesIO):
-    pass
-
-
-# def compress_photo(photo: np.ndarray, quality: int = 80) -> BytesIO:
-#     # Convert the image (NumPy array) to JPEG format with compression
-#     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
-#     is_success, encoded_image = cv2.imencode(".jpg", photo, encode_param)
-
-#     if is_success:
-#         # Convert the encoded image back to BytesIO to yield as a file-like object
-#         compressed_image = BytesIO(encoded_image.tobytes())
-#         return compressed_image
-#     else:
-#         raise ValueError("Image compression failed")
+    return image_enhanced
 
 
 async def extract_random_frames(video_file: UploadFile, number_of_frames: int = 1):
