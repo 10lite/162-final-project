4c68001d0b8fb950017a0bdaab159cd533812fe1
10lite <rcpaderna@up.edu.ph>
2024-11-30 22:50:07 +0800

    init: more FE init

---
+++ frontend/app/commute/page.tsx
@@ -0,0 +1,10 @@
+export default function Commute() {
+  return (
+    <div>
+      <h1>Commute</h1>
+    </div>
+  )
+}
+
+
+
--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -1,26 +1,36 @@
+'use client'
 import { Button } from "./ui/button"
-import { CarIcon, VideoIcon, ImageIcon, BarChartIcon } from "lucide-react"
-
+import { CarIcon, VideoIcon, BarChartIcon, InfoIcon, AlertTriangle } from "lucide-react"
+import { usePathname } from "next/navigation"
+import Link from "next/link"
 export default function Sidebar() {
+  const pathname = usePathname()
+
+  const menu_items = [
+    { icon: <InfoIcon className="mr-2 h-4 w-4" />, text: "About the App", path: "/about" },
+    { icon: <VideoIcon className="mr-2 h-4 w-4" />, text: "Analyze Traffic", path: "/" },
+    { icon: <BarChartIcon className="mr-2 h-4 w-4" />, text: "Traffic Patterns", path: "/map" },
+    { icon: <CarIcon className="mr-2 h-4 w-4" />, text: "Commute Guide", path: "/commute" },
+    { icon: <AlertTriangle className="mr-2 h-4 w-4" />, text: "Report an Incident", path: "/report" },
+  ]
+
   return (
     <aside className="w-64 border-r p-4">
+      <h1 className="text-2xl font-bold mb-4">BantayTrapiko</h1>
       <nav className="space-y-2">
-        <Button variant="ghost" className="w-full justify-start">
-          <VideoIcon className="mr-2 h-4 w-4" />
-          Video Analysis
-        </Button>
-        <Button variant="ghost" className="w-full justify-start">
-          <ImageIcon className="mr-2 h-4 w-4" />
-          Image Analysis
-        </Button>
-        <Button variant="ghost" className="w-full justify-start">
-          <CarIcon className="mr-2 h-4 w-4" />
-          Vehicle Tracking
-        </Button>
-        <Button variant="ghost" className="w-full justify-start">
-          <BarChartIcon className="mr-2 h-4 w-4" />
-          Traffic Patterns
-        </Button>
+        {menu_items.map(({ icon, text, path }) => (
+          <Button
+            asChild
+            key={text}
+            variant="ghost"
+            className={`w-full justify-start ${pathname === path ? 'bg-accent text-accent-foreground' : ''}`}
+          >
+            <Link href={path}>
+              {icon}
+              {text}
+            </Link>
+          </Button>
+        ))}
       </nav>
     </aside>
   )
