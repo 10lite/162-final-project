28e0acba0c8e18b994a7e0d094206fae25c93723
ASPactores <anakin.pactores@gmail.com>
2024-12-02 00:51:09 +0800

    feat: limit upload size to api

--- app/frame_selection/select_random_frames.py
+++ app/frame_selection/select_random_frames.py
@@ -1,7 +1,8 @@
 import base64
 
-from fastapi import HTTPException, UploadFile, APIRouter, File
+from fastapi import HTTPException, UploadFile, APIRouter, File, status
 from fastapi.responses import JSONResponse, StreamingResponse
+from enum import Enum
 from .utils import extract_random_frames, compress_photo, enhance_photo
 
 from fastapi import File, UploadFile
@@ -10,15 +11,28 @@ from fastapi import File, UploadFile
 router = APIRouter()
 
 
+class VideoUploadLimit(int, Enum):
+    TEN_MB = 10 * 1024 * 1024  # 10 MB
+
+
 @router.post("/get_enhanced_frames")
 async def select_random_frames(video: UploadFile = File(...)):
     frames = []
 
     try:
+        if video.size > VideoUploadLimit.TEN_MB:
+            raise HTTPException(
+                status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+                detail="File size is too large. Maximum file size is 10MB.",
+            )
+
         async for frame in extract_random_frames(video, number_of_frames=3):
             enhanced_image = enhance_photo(frame)
             compressed_frame = compress_photo(enhanced_image)
             frames.append(compressed_frame.getvalue())
+
+    except HTTPException as e:
+        raise e
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
