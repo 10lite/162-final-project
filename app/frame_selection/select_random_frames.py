0eea62a6a4108bdf818598817cb9e2385ee59ced
ASPactores <anakin.pactores@gmail.com>
2024-12-02 01:18:38 +0800

    docs: configured schema for select_random_frames

--- app/frame_selection/select_random_frames.py
+++ app/frame_selection/select_random_frames.py
@@ -1,11 +1,11 @@
 import base64
+from enum import Enum
 
 from fastapi import HTTPException, UploadFile, APIRouter, File, status
-from fastapi.responses import JSONResponse, StreamingResponse
-from enum import Enum
-from .utils import extract_random_frames, compress_photo, enhance_photo
+from pydantic import BaseModel
 
-from fastapi import File, UploadFile
+from .utils import extract_random_frames, compress_photo, enhance_photo
+from ..common.models import ErrorResponse
 
 
 router = APIRouter()
@@ -15,30 +15,47 @@ class VideoUploadLimit(int, Enum):
     TEN_MB = 10 * 1024 * 1024  # 10 MB
 
 
-@router.post("/get_enhanced_frames")
-async def select_random_frames(video: UploadFile = File(...)):
+class SelectRandomFramesResponse(BaseModel):
+    photos: list[str]
+
+
+error_response = {
+    413: {
+        "description": "File size is too large",
+        "model": ErrorResponse,
+    },
+    400: {
+        "description": "Bad request, general error",
+        "model": ErrorResponse,
+    },
+}
+
+
+@router.post(
+    "/get_enhanced_frames",
+    response_model=SelectRandomFramesResponse,
+    responses=error_response,
+)
+async def select_random_frames(
+    video: UploadFile = File(...),
+) -> SelectRandomFramesResponse:
     frames = []
 
-    try:
-        if video.size > VideoUploadLimit.TEN_MB:
-            raise HTTPException(
-                status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
-                detail="File size is too large. Maximum file size is 10MB.",
-            )
+    if video.size > VideoUploadLimit.TEN_MB:
+        raise HTTPException(
+            status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
+            detail="File size is too large. Maximum file size is 10MB.",
+        )
 
+    try:
         async for frame in extract_random_frames(video, number_of_frames=3):
             enhanced_image = enhance_photo(frame)
             compressed_frame = compress_photo(enhanced_image)
             frames.append(compressed_frame.getvalue())
 
-    except HTTPException as e:
-        raise e
-
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
 
-    # Convert the frames to base64 before returning as JSON
     frames_base64 = [base64.b64encode(f).decode() for f in frames]
 
-    # Return frames as a JSON response
-    return JSONResponse(content={"photos": frames_base64})
+    return SelectRandomFramesResponse(photos=frames_base64)
