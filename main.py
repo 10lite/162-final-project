6b0b9ec5d9b0101dd39abd12600541b3e2d28ee0
-m <villamorjohnnoel@gmail.com>
2024-12-10 16:14:27 +0800

    feat: add the car detection router in the app

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastapi.responses import HTMLResponse
 from fastapi import FastAPI
 import uvicorn
-from app.frame_selection.select_random_frames import router
+from app.frame_selection.select_random_frames import router as frame_selection_router
+from app.car_detection.detect_vehicles import router as car_detection_router
 
 app = FastAPI(
     title="CMSC 162 - Final Project",
 )
 
-app.include_router(router, prefix="/api")
+app.include_router(frame_selection_router, prefix="/api")
+app.include_router(car_detection_router, prefix="/api")
 
 
 @app.get("/", include_in_schema=False)
